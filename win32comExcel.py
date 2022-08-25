8a7ae379f3ed406d2b84fae7688e80676617a019
Belkrane <jonghagi77@gmail.com>
2022-08-25 13:00:51 +0900

    Excel Cell Contorl and Save, Load practice

--- win32comExcel.py
+++ win32comExcel.py
@@ -5,3 +5,12 @@ excel = win32com.client.Dispatch("Excel.Application")
 excel.Visible = True
 wb = excel.WorkBooks.Add()
 ws = wb.WorkSheets("Sheet1")
+ws.Cells(1,1).Value = "Hello World"
+
+wb.SaveAs('./test.xlsx')
+excel.Quit()
+
+wb = excel.WorkBooks.Open('./test.xlsx')
+ws = wb.ActiveSheet
+print(ws.Cells(1,1).Value)
+excel.Quit()
