b2af789cbb660a64dc4f11ca4d0a3337acd39057
Belkrane <jonghagi77@gmail.com>
2022-08-28 19:26:30 +0900

    Coloring Cells

--- win32comExcel.py
+++ win32comExcel.py
@@ -3,14 +3,10 @@ import win32com.client
 excel = win32com.client.Dispatch("Excel.Application")
 
 excel.Visible = True
-wb = excel.WorkBooks.Add()
-ws = wb.WorkSheets("Sheet1")
-ws.Cells(1,1).Value = "Hello World"
-
-wb.SaveAs('./test.xlsx')
-excel.Quit()
-
-wb = excel.WorkBooks.Open('./test.xlsx')
+wb = excel.WorkBooks.Open("./practice.xlsx")
 ws = wb.ActiveSheet
 print(ws.Cells(1,1).Value)
-excel.Quit()
+
+ws.Cells(1,2).Value = "is"
+ws.Range("C1").Value = "Good"
+ws.Range("C1").Interior.ColorIndex = 10
